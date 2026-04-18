4e71018184dfc601c2d5b26f6a9f0396b42137dd
kurehajime <xiidec@gmail.com>
2026-04-19 00:29:00 +0900

    Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: repositoryName ? `/${repositoryName}/` : '/',
   plugins: [react()],
 })
